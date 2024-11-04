512202e97a0bf3c09bcf3a16fab69da8ed0be9fb
Rodney Willems <rodneywillems@gmail.com>
2024-11-05 00:19:55 +0100

    Made enemy move up and down

--- Assets/Scripts/Enemies/Moving Enemy/Towards Player.cs
+++ Assets/Scripts/Enemies/Moving Enemy/Towards Player.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class TowardsPlayer : MovingEnemy
 {
+    private Vector3 upOrDown;
+
+    protected override void Start()
+    {
+        base.Start();
+        upOrDown = Vector3.down;
+    }
+
     protected override void Moving()
     {
         base.Moving();
-        transform.position = Vector3.Lerp(transform.position, transform.position + Vector3.down, 1 * Time.deltaTime);
-        if (transform.position.y < -7)
+        transform.position = Vector3.Lerp(transform.position, transform.position + upOrDown, 0.75f * Time.deltaTime);
+        if (transform.position.y < -5)
         {
-            Destroy(gameObject);
+            upOrDown = Vector3.up;
+        }
+        else if (transform.position.y > 5)
+        {
+            upOrDown = Vector3.down;
         }
     }
 }
